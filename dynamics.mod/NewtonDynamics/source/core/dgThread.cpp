43184e28374e9f1bdaaead6ef3ede79d5a36a5e1
woollybah <woollybah@gmail.com>
2016-08-17 00:16:17 +0100

    Register threads with BlitzMax.

--- dynamics.mod/NewtonDynamics/source/core/dgThread.cpp
+++ dynamics.mod/NewtonDynamics/source/core/dgThread.cpp
@@ -121,6 +121,9 @@ void* dgThread::dgThreadSystemCallback(void* threadData)
 #ifdef BMX_NG
 #define GC_THREADS
 #include "brl.mod/blitz.mod/bdwgc/include/gc.h"
+extern "C" {
+#include "brl.mod/blitz.mod/blitz.h"
+}
 #endif
 
 #if defined (_MACOSX_VER) || defined (IOS) || defined (__APPLE__)
@@ -243,19 +246,27 @@ void* dgThread::dgThreadSystemCallback(void* threadData)
 	dgFloatExceptions exception;
 	dgSetPrecisionDouble precision;
 
+	dgThread* const me = (dgThread*) threadData;
+
 #ifdef BMX_NG
 	struct GC_stack_base base;
 	GC_get_stack_base(&base);
 	GC_register_my_thread(&base);
+
+#ifdef _WIN32
+	BBThread * thd = bbThreadRegister(me->m_id);
+#else
+	BBThread * thd = bbThreadRegister(me->m_handle);
+#endif
 #endif
 
-	dgThread* const me = (dgThread*) threadData;
 	dgInterlockedExchange(&me->m_threadRunning, 1);
 	me->Execute(me->m_id);
 	dgInterlockedExchange(&me->m_threadRunning, 0);
 	dgThreadYield();
 
 #ifdef BMX_NG
+	bbThreadUnregister(thd);
 	GC_unregister_my_thread();
 #endif
 
